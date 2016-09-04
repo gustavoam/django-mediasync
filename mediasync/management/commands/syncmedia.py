7b3f08e28edd8adf8dcc8781f1e3b0d9c8647346
Peter Sanchez <peter@netlandish.com>
2016-09-04 09:56:53 -0700

    Fixing management command for modern Django API

--- mediasync/management/commands/syncmedia.py
+++ mediasync/management/commands/syncmedia.py
@@ -4,22 +4,28 @@ from mediasync.conf import msettings
 import mediasync
 
 class Command(BaseCommand):
-    
+
     help = "Sync local media with remote client"
     args = '[options]'
-    
+
     requires_model_validation = False
-    
-    option_list = BaseCommand.option_list + (
-        make_option("-F", "--force", dest="force", help="force files to sync", action="store_true"),
-    )
-    
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-F', '--force',
+            dest='force',
+            action='store_true',
+            default=False,
+            help='force files to sync',
+
+        )
+
     def handle(self, *args, **options):
-        
+
         msettings['SERVE_REMOTE'] = True
-        
+
         force = options.get('force') or False
-        
+
         try:
             mediasync.sync(force=force)
         except ValueError, ve:
